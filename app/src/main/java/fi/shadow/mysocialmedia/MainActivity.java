66ea08fdeddf8ef50192b35267a986234592a304
JonsQu <joonatan.haavisto@tuni.fi>
2020-03-31 09:51:14 +0300

    Modified to be only welcome screen to the app and login's user to Facebook.

--- app/src/main/java/fi/shadow/mysocialmedia/MainActivity.java
+++ app/src/main/java/fi/shadow/mysocialmedia/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
+import com.facebook.BuildConfig;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -40,20 +41,30 @@ public class MainActivity extends AppCompatActivity {
             FacebookSdk.setIsDebugEnabled(true);
             FacebookSdk.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
         }
-        Log.d("MainActivity", String.valueOf(AccessToken.getCurrentAccessToken()));
+        callbackManager = CallbackManager.Factory.create();
+        //Log.d("MainActivity", String.valueOf(AccessToken.getCurrentAccessToken()));
         if(AccessToken.getCurrentAccessToken() != null){
             accessToken = AccessToken.getCurrentAccessToken();
-            getProfile();
+            goToFaceProfile();
+        }else{
+            loginFacebook();
         }
-        callbackManager = CallbackManager.Factory.create();
         accessTokenTracker = new AccessTokenTracker() {
             @Override
             protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
                 accessToken = currentAccessToken;
             }
         };
-        accessToken = AccessToken.getCurrentAccessToken();
-        LoginButton loginButton = findViewById(R.id.loginButton);
+        //accessToken = AccessToken.getCurrentAccessToken();
+        //LoginButton loginButton = findViewById(R.id.loginButton);
+
+    }
+    public void goToFaceProfile(){
+        Intent profile = new Intent(this, MainPageActivity.class);
+        profile.putExtra("accessToken", accessToken);
+        startActivity(profile);
+    }
+    public void loginFacebook(){
         LoginManager.getInstance().registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
@@ -62,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("MainActivity", Profile.getCurrentProfile().getFirstName());
                         accessToken = loginResult.getAccessToken();
                         getProfile();
+                        goToFaceProfile();
                     }
 
                     @Override
@@ -89,32 +101,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-//Forward onActivityResult to the callbackManager//
-
+        //Forward onActivityResult to the callbackManager//
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
     private void getProfile(){
-        TextView myName = findViewById(R.id.myName);
+        //TextView myName = findViewById(R.id.myName);
         GraphRequest request = GraphRequest.newMeRequest(
                 accessToken,
-                new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
-                        // Insert your code here
-                        Log.d("MainActivity", Profile.getCurrentProfile().getFirstName());
-                        Log.d("MainActivity", Profile.getCurrentProfile().getProfilePictureUri(200,200).toString());
-                        Log.d("MainActivity", response.toString());
-                        Log.d("MainActivity", object.toString());
-                        try {
-                            userId = object.getString("id");
-                            myName.setText(object.getString("name"));
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        getFeed();
-                        Log.d("MainActivity", String.valueOf(object));
+                (object, response) -> {
+                    // Insert your code here
+                    //Log.d("MainActivity", Profile.getCurrentProfile().getFirstName());
+                    //Log.d("MainActivity", Profile.getCurrentProfile().getProfilePictureUri(200,200).toString());
+                    //Log.d("MainActivity", response.toString());
+                    //Log.d("MainActivity", object.toString());
+                    try {
+                        userId = object.getString("id");
+                        //myName.setText(object.getString("name"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
+                    //getFeed();
+                    Log.d("MainActivity", String.valueOf(object));
                 });
 
         Bundle parameters = new Bundle();
